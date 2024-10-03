a5b2c5f811997643b59498c3cbc3902664ab05ae
monkofmustang <monkofmustang@gmail.com>
2024-10-03 23:24:43 +0530

    hardcoded image for online khabar

--- online_khabar.py
+++ online_khabar.py
@@ -57,7 +57,7 @@ def ok_en():
                 "link": entry.link,
                 "pubDate": entry.published,
                 "category": entry.category,
-                "image": image_url,
+                "image": "https://english.onlinekhabar.com/wp-content/uploads/2020/09/cropped-logo-footer-32x32.png",
                 "publisher": 'Online Khabar'
             })
         return news_items
