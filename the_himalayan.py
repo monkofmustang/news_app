ea357922a30d45c819121523c682817ae481eee9
monkofmustang <monkofmustang@gmail.com>
2024-10-04 10:37:12 +0530

    cleaned title of THT

--- the_himalayan.py
+++ the_himalayan.py
@@ -2,6 +2,7 @@ import feedparser
 from fastapi import HTTPException
 import os
 from dotenv import load_dotenv
+import html
 
 load_dotenv()
 
@@ -12,8 +13,9 @@ def ok_en():
         ok_nep_feeds = feedparser.parse(rss_url)
         news_items = []
         for entry in ok_nep_feeds.entries:
+            news_title = clean_title(entry.title)
             news_items.append({
-                "title": entry.title,
+                "title": news_title,
                 "description": entry.title_detail.value,
                 "content": entry.summary_detail.value,
                 "link": entry.link,
@@ -28,3 +30,13 @@ def ok_en():
     # news_json = json.dumps(news_items, ensure_ascii=False, indent=4)
     # # print(news_json)
     # return news_json
+
+
+def clean_title(news_item):
+    # Decode HTML entities in a loop until all are unescaped
+    while True:
+        decoded_title = html.unescape(news_item)
+        if decoded_title == news_item:  # Stop when no further decoding is needed
+            break
+        news_item = decoded_title
+    return news_item
