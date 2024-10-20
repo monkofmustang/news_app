88a606e1e382cf3afe5d3a37b48a760eede5def6
monkofmustang <monkofmustang@gmail.com>
2024-10-20 22:15:55 +0530

    image corected of news of nepal

--- newsFeeds/news_of_nepal.py
+++ newsFeeds/news_of_nepal.py
@@ -17,7 +17,7 @@ def non_np():
             content_value = entry.content[0].value if hasattr(entry.content, '__getitem__') and len(
                 entry.content) > 0 else None
             image_url_match = re.search(r'(https?://[^\s]+(?:\.jpeg|\.jpg))', content_value)
-            image_url = image_url_match.group(0) if image_url_match else None
+            image_url = image_url_match.group(0).replace("-150x150","") if image_url_match else None
             soup = BeautifulSoup(content_value, "html.parser")
             content_text = soup.get_text(separator="\n")
             news_items.append({
