e8c6d8861b6c7c464197d3ed4f4c7488c36ae806
Admin <monkofmustang@gmail.com>
2025-01-08 10:23:50 +0530

    commented db impl

--- controller/subscriber_controller.py
+++ controller/subscriber_controller.py
@@ -7,16 +7,16 @@ router = APIRouter(
     tags=["Subscribers"],  # Tag for grouping in the documentation
 )
 
-collection = get_collection("subscription")
+# collection = get_collection("subscription")
 
 
-@router.post("/")
-async def create_subscriber(subscriber: Subscribers):
-    # Check if the subscriber already exists
-    existing_subscriber = await collection.find_one({"mobNumber": subscriber.mobNumber})
-    if existing_subscriber:
-        raise HTTPException(status_code=400, detail="Subscriber already exists.")
-
-    # Insert the subscriber into MongoDB
-    result = await collection.insert_one(subscriber.dict())
-    return {"id": str(result.inserted_id), "message": "Subscriber created successfully."}
+# @router.post("/")
+# async def create_subscriber(subscriber: Subscribers):
+#     # Check if the subscriber already exists
+#     existing_subscriber = await collection.find_one({"mobNumber": subscriber.mobNumber})
+#     if existing_subscriber:
+#         raise HTTPException(status_code=400, detail="Subscriber already exists.")
+#
+#     # Insert the subscriber into MongoDB
+#     result = await collection.insert_one(subscriber.dict())
+#     return {"id": str(result.inserted_id), "message": "Subscriber created successfully."}
--- db/db_connection.py
+++ db/db_connection.py
@@ -3,12 +3,12 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-
-client = AsyncIOMotorClient(
-    os.getenv("DB_CONNECTION")
-)
-db = client["news"]
-
-
-def get_collection(collection_name: str):
-    return db[collection_name]
+#
+# client = AsyncIOMotorClient(
+#     os.getenv("DB_CONNECTION")
+# )
+# db = client["news"]
+#
+#
+# def get_collection(collection_name: str):
+#     return db[collection_name]
